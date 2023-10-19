b55b8a880f40e75622f017e90a7bb2a6acf5fa6d
ismailkhlif <ismail.khlif@esprit.tn>
2023-10-19 19:18:28 +0100

    Etudiant Module

--- src/main/java/com/spring/kaddem/entities/Etudiant.java
+++ src/main/java/com/spring/kaddem/entities/Etudiant.java
@@ -23,10 +23,92 @@ public class Etudiant  implements Serializable {
     private String prenomE;
     private String nomE;
     @Enumerated(EnumType.STRING)
-    private Option op;
+    private  Option op;
 
 
 
+
+    @Override
+    public String toString() {
+        return "Etudiant{" +
+                "idEtudiant=" + idEtudiant +
+                ", prenomE='" + prenomE + '\'' +
+                ", nomE='" + nomE + '\'' +
+                ", op=" + op +
+                ", departement=" + departement +
+                ", equipes=" + equipes +
+                ", contrats=" + contrats +
+                '}';
+    }
+
+    public Etudiant() {
+    }
+
+    public Etudiant(Integer idEtudiant, String prenomE, String nomE, Option op, Departement departement, List<Equipe> equipes, List<Contrat> contrats) {
+        this.idEtudiant = idEtudiant;
+        this.prenomE = prenomE;
+        this.nomE = nomE;
+        this.op = op;
+        this.departement = departement;
+        this.equipes = equipes;
+        this.contrats = contrats;
+    }
+
+    public Integer getIdEtudiant() {
+        return idEtudiant;
+    }
+
+    public void setIdEtudiant(Integer idEtudiant) {
+        this.idEtudiant = idEtudiant;
+    }
+
+    public String getPrenomE() {
+        return prenomE;
+    }
+
+    public void setPrenomE(String prenomE) {
+        this.prenomE = prenomE;
+    }
+
+    public String getNomE() {
+        return nomE;
+    }
+
+    public void setNomE(String nomE) {
+        this.nomE = nomE;
+    }
+
+    public Option getOp() {
+        return op;
+    }
+
+    public void setOp(Option op) {
+        this.op = op;
+    }
+
+    public Departement getDepartement() {
+        return departement;
+    }
+
+    public void setDepartement(Departement departement) {
+        this.departement = departement;
+    }
+
+    public List<Equipe> getEquipes() {
+        return equipes;
+    }
+
+    public void setEquipes(List<Equipe> equipes) {
+        this.equipes = equipes;
+    }
+
+    public List<Contrat> getContrats() {
+        return contrats;
+    }
+
+    public void setContrats(List<Contrat> contrats) {
+        this.contrats = contrats;
+    }
 
             @ManyToOne
             @JsonIgnore
@@ -37,7 +119,6 @@ public class Etudiant  implements Serializable {
     @OneToMany(mappedBy = "etudiant")
     @JsonIgnore
     private List<Contrat> contrats;
-
 
 
 }
--- src/main/java/com/spring/kaddem/repositories/EtudiantRepository.java
+++ src/main/java/com/spring/kaddem/repositories/EtudiantRepository.java
@@ -1,14 +1,62 @@
 package com.spring.kaddem.repositories;
 
 
-import com.spring.kaddem.entities.Etudiant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import tn.esprit.spring.khaddem.entities.Contrat;
+import tn.esprit.spring.khaddem.entities.Etudiant;
+import tn.esprit.spring.khaddem.entities.Niveau;
+import tn.esprit.spring.khaddem.entities.Specialite;
 
+import java.util.List;
 
 @Repository
 public interface EtudiantRepository extends JpaRepository<Etudiant, Integer> {
 
 
-    Etudiant findByNomEAndPrenomE(String nomE, String prenomE);
+    //public List<Etudiant> findByDepartementIdDepartement(Long idDepartement);
+    //public List<Etudiant> findByEquipesNiveau(Niveau nv);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    List<Etudiant> findByDepartementIdDepartement(Integer idDepartement);
+
+   List<Etudiant> findByEquipesNiveau(Niveau niveau);
+
+   Etudiant findByNomEAndPrenomE(String nomE, String prenomE);
+
+
+
+
+
+
+    @Query("SELECT e FROM Etudiant e, Contrat c where "
+           + "e.idEtudiant = c.etudiant.idEtudiant "
+           + "and c.specialite =:specialite ")
+   List<Etudiant> retrieveEtudiantsByContratSpecialite(@Param("specialite") Specialite specialite);
+
+ //  @Query(value = "SELECT * FROM etudiant e INNER JOIN contrat c ON e.id_etudiant =   c.etudiant_id_etudiant where c.specialite = :specialite", nativeQuery = true)
+  // List<Etudiant> retrieveEtudiantsByContratSpecialiteSQL(@Param("specialite") Specialite specialite);
+ @Query(value = "SELECT * FROM etudiant e INNER JOIN contrat c ON e.id_etudiant =   c.etudiant_id_etudiant where c.specialite =:specialite", nativeQuery = true)
+ List<Etudiant> retrieveEtudiantsByContratSpecialiteSQL(@Param("specialite") String specialite);
+
+
+
 }
