2693f05d95f4c0249e699817b887141e03e30c23
Ismail-khlif <93547849+Ismail-khlif@users.noreply.github.com>
2023-10-20 21:19:51 +0100

    Update EtudiantRestController.java

--- src/main/java/com/spring/kaddem/controllers/EtudiantRestController.java
+++ src/main/java/com/spring/kaddem/controllers/EtudiantRestController.java
@@ -34,13 +34,13 @@ public class EtudiantRestController {
     @PostMapping("/add-etudiant")
     @ResponseBody
     public Etudiant addEtudiant(@RequestBody Etudiant e) {
-        return etudiantService.addEtudiant(e); 
+        return etudiantService.addOrUpdateEtudiant(e); 
     }
 
     @PutMapping("/update-etudiant")
     @ResponseBody
     public Etudiant updateEtudiant(@RequestBody Etudiant e) {
-        return etudiantService.updateEtudiant(e);
+        return etudiantService.addOrUpdateEtudiant(e);
     }
     @DeleteMapping("/removeEtudiant/{idEtudiant}")
     @ResponseBody
