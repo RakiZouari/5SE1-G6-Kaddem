d2a2ea0ad9eb520a44470e392960eeb69929c20e
Ismail-khlif <93547849+Ismail-khlif@users.noreply.github.com>
2023-10-22 09:47:48 +0100

    Update EtudiantServiceImpl.java

--- src/main/java/com/spring/kaddem/services/EtudiantServiceImpl.java
+++ src/main/java/com/spring/kaddem/services/EtudiantServiceImpl.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.spring.kaddem.entities.*;
+import com.spring.kaddem.dto.EtudiantDto;
 import com.spring.kaddem.repositories.*;
 import java.util.Optional;
 import javax.transaction.Transactional;
@@ -30,7 +31,7 @@ public class EtudiantServiceImpl implements IEtudiantService{
     }
 
     @Override
-    public Etudiant addOrUpdateEtudiant(Etudiant e) {
+    public EtudiantDto addOrUpdateEtudiant(EtudiantDto e) {
         etudiantRepository.save(e);
         return e;
     }
@@ -85,7 +86,7 @@ public class EtudiantServiceImpl implements IEtudiantService{
     }
 
     @Transactional
-    public Etudiant addAndAssignEtudiantToEquipeAndContract(Etudiant e, Integer idContrat, Integer idEquipe) {
+    public EtudiantDto addAndAssignEtudiantToEquipeAndContract(EtudiantDto e, Integer idContrat, Integer idEquipe) {
         Optional<Contrat> contratOptional = contratRepository.findById(idContrat);
         Optional<Equipe> equipeOptional = equipeRepository.findById(idEquipe);
     
