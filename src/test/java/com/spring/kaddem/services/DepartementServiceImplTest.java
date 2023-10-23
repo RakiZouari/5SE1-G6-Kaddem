36af55082e9c19c0d17a98f5b91ce1fde2e66334
Rihab HAMOUDI <rihab.hamoudi@esprit.tn>
2023-10-23 20:11:13 +0100

    sonarcorrection  push

---
+++ src/test/java/com/spring/kaddem/services/DepartementServiceImplTest.java
@@ -0,0 +1,30 @@
+package com.spring.kaddem.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+
+class DepartementServiceImplTest {
+
+    @Test
+    void ajouterdepartement() {
+    }
+
+    @Test
+    void getAllD() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
