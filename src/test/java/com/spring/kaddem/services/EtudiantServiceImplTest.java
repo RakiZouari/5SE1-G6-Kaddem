359804df7383d354440f365c53d2dea94ed9b34f
Ismail-khlif <93547849+Ismail-khlif@users.noreply.github.com>
2023-10-23 18:10:31 +0100

    Update EtudiantServiceImplTest.java

--- src/test/java/com/spring/kaddem/services/EtudiantServiceImplTest.java
+++ src/test/java/com/spring/kaddem/services/EtudiantServiceImplTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.assertEquals;
