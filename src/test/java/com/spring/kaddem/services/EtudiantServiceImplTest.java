e5382ddcab789adf2ff74ce1fd66ffee07e579ad
Ismail-khlif <93547849+Ismail-khlif@users.noreply.github.com>
2023-11-03 15:36:28 +0100

    Update EtudiantServiceImplTest.java

--- src/test/java/com/spring/kaddem/services/EtudiantServiceImplTest.java
+++ src/test/java/com/spring/kaddem/services/EtudiantServiceImplTest.java
@@ -488,7 +488,7 @@ void testAddAndAssignEtudiantToEquipeAndContract_EtudiantEquipesNotNull() {
 
     when(contratRepository.findById(idContrat)).thenReturn(Optional.of(new Contrat()));
     when(equipeRepository.findById(idEquipe)).thenReturn(Optional.of(new Equipe()));
-    when(etudiantRepository.save(any(Etudiant.class)).thenReturn(new Etudiant());
+    when(etudiantRepository.save(any(Etudiant.class))).thenReturn(new Etudiant());
 
     Etudiant etudiant = new Etudiant();
     Equipe equipe = new Equipe();
