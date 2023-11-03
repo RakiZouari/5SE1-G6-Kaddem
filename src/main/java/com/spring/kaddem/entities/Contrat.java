e420ca3fc381d12ad24e402e903b602c7a42774c
Ismail-khlif <93547849+Ismail-khlif@users.noreply.github.com>
2023-11-03 12:09:45 +0100

    Update Contrat.java

--- src/main/java/com/spring/kaddem/entities/Contrat.java
+++ src/main/java/com/spring/kaddem/entities/Contrat.java
@@ -3,7 +3,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
