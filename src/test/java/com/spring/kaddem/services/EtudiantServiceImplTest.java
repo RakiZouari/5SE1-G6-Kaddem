69d1c5b187c9a70f4850ac8a67f1261367c7392a
Ismail-khlif <93547849+Ismail-khlif@users.noreply.github.com>
2023-11-03 16:39:38 +0100

    Update EtudiantServiceImplTest.java

--- src/test/java/com/spring/kaddem/services/EtudiantServiceImplTest.java
+++ src/test/java/com/spring/kaddem/services/EtudiantServiceImplTest.java
@@ -478,35 +478,4 @@ void testAddAndAssignEtudiantToEquipeAndContract_BothNotFound() {
 
     verify(etudiantRepository, never()).save(any(Etudiant.class));
 }
-
-@Test
-void testAddAndAssignEtudiantToEquipeAndContract_EtudiantEquipesNotNull() {
-    int idContrat = 1;
-    int idEquipe = 2;
-    EtudiantDto etudiantDto = new EtudiantDto();
-    Etudiant et = new Etudiant();
-    when(contratRepository.findById(idContrat)).thenReturn(Optional.of(new Contrat()));
-    when(equipeRepository.findById(idEquipe)).thenReturn(Optional.of(new Equipe()));
-    when(etudiantRepository.save(any(Etudiant.class))).thenReturn(et);
-    et.setIdEtudiant(1);
-
-    Etudiant etudiant = new Etudiant();
-    Equipe equipe = new Equipe();
-    equipe.setIdEquipe(idEquipe);
-    List<Equipe> equipes = new ArrayList<>();
-    equipes.add(equipe);
-    etudiant.setEquipes(equipes);
-
-    when(etudiantRepository.findById(etudiant.getIdEtudiant())).thenReturn(Optional.of(etudiant));
-
-    EtudiantDto result = etudiantService.addAndAssignEtudiantToEquipeAndContract(etudiantDto, idContrat, idEquipe);
-
-    verify(etudiantRepository).save(any(Etudiant.class));
-
-    assertEquals(1, result.getIdEtudiant());
-    assertEquals(1, result.getEquipes().size());
-}
-
-
-
 }
