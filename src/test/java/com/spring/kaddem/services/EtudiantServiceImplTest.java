fc3aecd49885f86b617deb1f324389efdea635ef
Ismail-khlif <93547849+Ismail-khlif@users.noreply.github.com>
2023-11-04 20:10:11 +0100

    Update EtudiantServiceImplTest.java

--- src/test/java/com/spring/kaddem/services/EtudiantServiceImplTest.java
+++ src/test/java/com/spring/kaddem/services/EtudiantServiceImplTest.java
@@ -734,7 +734,8 @@ void testAddAndAssignEtudiantToEquipeAndContract_BothNotFound() {
         EtudiantDto result = etudiantService.addAndAssignEtudiantToEquipeAndContract(etudiantDto, 2, 1);
         
         mockMvc.perform(MockMvcRequestBuilders.post("/etudiant/addAndAssignEtudiantToEquipeAndContract/1/2")
-                .contentType(MediaType.APPLICATION_JSON))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(etudiantDto)))
                 .andExpect(status().isOk());
     }
 
