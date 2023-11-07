8f1afe073387446f187ef43607230f288002ae99
Mariem <mariem.haddad@esprit.tn>
2023-11-07 18:12:37 +0100

    Pom xml

--- src/test/java/com/spring/kaddem/UniversiteRepositoryTest.java
+++ src/test/java/com/spring/kaddem/UniversiteRepositoryTest.java
@@ -1,7 +1,19 @@
 package com.spring.kaddem;
 
+import com.spring.kaddem.repositories.UniversiteRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
+@ExtendWith(SpringExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest
+@Slf4j
 public class UniversiteRepositoryTest {
+    @Autowired
+    UniversiteRepository universiteRepository;
 }
